e46294ecc01aba123fd4fe9f34045ff837e95169
Mislav Znidarec <znidarec01@gmail.com>
2024-03-21 21:02:46 +0100

    final commit
    zeznulo me je u pocetku parsanje podataka, nikada nisam radio na tako velikom scope-u podataka (najcesce su bili manji i targetani samo za tablicu da se pokaze), fali orderanje i dosta toga, nazalost nisam znao kako da to rjesim u pocetku s tako velikim scopeom podataka
    
    jedino sto nije napravljeno je responzivni dizajn, zbir golova i datan styling

--- eval-task/src/app/app.module.ts
+++ eval-task/src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 import { MatCommonModule } from '@angular/material/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatCardModule } from '@angular/material/card';
-import {MatExpansionModule} from '@angular/material/expansion';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
 
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
@@ -23,18 +26,19 @@ import { RouterModule } from '@angular/router';
     MatToolbarModule,
     HttpClientModule,
     MatCommonModule,
-    MatCardModule,MatFormFieldModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatListModule,
+    MatIconModule,
     MatExpansionModule,
     MatTableModule,
     MatGridListModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{ path: '', component: TableComponent}])
+    RouterModule.forRoot([{ path: '', component: TableComponent }]),
   ],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    TableComponent,
-  ],
+  declarations: [AppComponent, TopBarComponent, TableComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- eval-task/src/app/table/table.component.ts
+++ eval-task/src/app/table/table.component.ts
@@ -1,46 +1,114 @@
 import { HttpClient } from '@angular/common/http';
-import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
-import { MatAccordion } from '@angular/material/expansion';
-import { Location } from './../../shared/model/model';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
-const ELEMENT_DATA: any[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrl: './table.component.scss'
+  styleUrl: './table.component.scss',
 })
 export class TableComponent {
-  // dataSource !: Location[];
-  displayedColumns: string[] = ['participants', 'startDate', '1', 'X', '2', '12', '1X', 'X2'];
+  displayedColumns: string[] = [
+    'participants',
+    'startDate',
+    '1',
+    'X',
+    '2',
+    '12',
+    '1X',
+    'X2',
+  ];
   // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource !: any[];
-  test !: any[];
+  dataSource!: any[];
+  test!: any[];
 
-  constructor(private http: HttpClient, private cdref: ChangeDetectorRef) {}
+  selectedValues: any[] = [];
+  numberValue: number = 0;
+  result: number = 0;
+  totalOdds: number = 0;
+
+  // private selectedValuesSubject = new BehaviorSubject<
+  //   { id: number; value: any; index: number }[]
+  // >([]);
+  // public selectedValues$ = this.selectedValuesSubject.asObservable();
+
+  constructor(
+    private http: HttpClient,
+    private cdref: ChangeDetectorRef,
+    private _snackBar: MatSnackBar
+  ) {}
 
   ngOnInit() {
-    this.http.get<any>('../../assets/offer.json')
-      .subscribe(data => {
-        // this.dataSource = data.locations;
-        this.test = data.locations;
-        this.dataSource = ELEMENT_DATA;
-        console.log(data.locations[0].leagues[0].eventDateGroups);
-        this.cdref.detectChanges();
-      });
+    this.http.get<any>('../../assets/offer.json').subscribe((data) => {
+      // this.dataSource = data.locations;
+      this.test = data.locations;
+      this.cdref.detectChanges();
+    });
   }
 
   formatParticipantNames(participants: any) {
     return `${participants.fixture.participants[0].name} <br> ${participants.fixture.participants[1].name}`;
+  }
+
+  onClick(event: any, value: any, index: any) {
+    const id = event.id;
+    const participants = event.fixture.participants;
+
+    if (this.selectedValues.length === 0) {
+      this.selectedValues.push({ id, value, index, participants });
+      this.selectedValues = [...this.selectedValues];
+      return;
+    }
+
+    if (this.selectedValues.length === 8) {
+      this._snackBar.open('Cannonball!!', 'Splash', {
+        horizontalPosition: 'end',
+        verticalPosition: 'top',
+      });
+    }
+
+    const existingIndex = this.selectedValues.findIndex(
+      (item) => item.id === id
+    );
+    if (existingIndex !== -1) {
+      if (
+        this.selectedValues[existingIndex].value !== value ||
+        this.selectedValues[existingIndex].index !== index
+      ) {
+        this.selectedValues[existingIndex].value = value;
+        this.selectedValues[existingIndex].index = index;
+      } else if (this.selectedValues[existingIndex].value === value) {
+        this.selectedValues.splice(existingIndex, 1);
+      }
+    } else {
+      this.selectedValues.push({ id, value, index, participants });
+    }
+    this.selectedValues = [...this.selectedValues];
+    this.cdref.detectChanges();
+  }
+
+  calculateValue() {
+    if (this.selectedValues.length === 0) {
+      return;
+    }
+    this.result = this.selectedValues.reduce((acc, curr) => {
+      return acc * curr.value;
+    }, 1);
+
+    this.totalOdds = Math.round(this.result * 100) / 100;
+
+    this.result = Math.round(this.totalOdds * this.numberValue * 100) / 100;
+  }
+
+  onDelete(event: any) {
+    this.selectedValues = this.selectedValues.filter(
+      (item) => item.id !== event
+    );
+    this.selectedValues = [...this.selectedValues];
+    this.cdref.detectChanges();
+  }
+
+  isCellClicked(id: any, index: any): boolean {
+    return this.selectedValues.some(cell => cell.id === id && cell.index === index);
   }
 }
